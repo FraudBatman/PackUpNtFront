88033f08ea7fe691ab457bb50ba838c9cc034585
Nicholas Ott <nicsonfire4@gmail.com>
2023-03-01 17:10:27 -0500

    began work on @octokit stuff instead of octokit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,6 @@
     export let data: PageData;
 </script> -->
 <script lang="ts">
-	import { user } from '$lib/sessionStore';
-
-	function userTest(): void {
-		console.log(user);
-		let userName: string = '';
-
-		if (user != undefined && user.identities != undefined) {
-			let userName = user?.identities[0]?.identity_data['user_name'];
-		} else {
-			return;
-		}
-	}
 </script>
 
 <h1>Home</h1>
@@ -23,4 +11,3 @@
 	A helpful, convenient toolkit that serves to keep packages optimized to the user's preferences and
 	send email notifications to remind them to keep things up to date.
 </b>
-<button on:click={userTest}>ass</button>
--- src/routes/packages/+page.svelte
+++ src/routes/packages/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Packages</h1>
+<script lang="ts">
+	import { user } from '$lib/sessionStore';
+
+	function userTest(): void {
+		console.log(user);
+		let userName: string = '';
+
+		if (user != undefined && user.identities != undefined) {
+			let userName = user?.identities[0]?.identity_data['user_name'];
+		} else {
+			return;
+		}
+	}
+</script>
+
+<main>
+	<h1>Packages</h1>
+	<button on:click={userTest}>ass</button>
+</main>
