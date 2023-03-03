2327f121fc02a3697551b072fc7f8947ddd421fe
Jason Paek <adventuredonut343@gmail.com>
2023-03-02 20:02:26 -0500

    Jumbotron home page rework

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,13 @@
 </script>
 
 <h1>Home</h1>
-<b style="font-size: 40px">
-	A helpful, convenient toolkit that serves to keep packages optimized to the user's preferences and
-	send email notifications to remind them to keep things up to date.
-</b>
+<div class="jumbotron">
+    <h1 class="display-4">Welcome to P.U.N.</h1>
+    <p class="lead">A helpful, convenient toolkit that serves to keep packages optimized to the user's preferences and
+        send email notifications to remind them to keep things up to date.</p>
+    <hr class="my-4">
+    <p>A Package Update Notifier that can make developer lives easier!</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+  </div>
