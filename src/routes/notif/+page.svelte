6a4970fae9f98861b7a69f40e700dbc6d673ad5e
Erik Smith <eriksmith98@gmail.com>
2023-03-28 18:24:05 -0400

    backend_response test function

--- src/routes/notif/+page.svelte
+++ src/routes/notif/+page.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
     import { supabase } from "$lib/supabaseClient";
-
+    
     async function test() {
         let { data: backend_reponse, error } = await supabase
-            .from('backend_reponse')
-            .select('*')
-    console.log(data);
-    
-    //add this to only get specific repo
-    // .eq('repo_id', getTheRepoIdAndPutInAsAVariableHere)
+        .from('backend_reponse')
+        .select('*')
+
+        //add this to only get specific repo
+        // .eq('repo_id', 123456789)
+        console.log(backend_reponse);
+
+        return backend_reponse;
     }
+
+    //create a new repo_setting row
+    const theRepoId = "";
+    const email = "";
+    const theExcludedPackages = "";
+    const notifreq = "";
+    const notifday = "";
+    async function createNewSetting() {
+        const { data, error } = await supabase
+        .from('repo_settings')
+        .insert([
+        { repo_id: theRepoId, email: email, excluded_packages: theExcludedPackages, notification_frequency: notifreq, notification_day : notifday  },
+        ])
+    }
+
+    test();
+    
 
 </script>
 
@@ -18,7 +37,7 @@
     <p class="lead fs-1" style="color: white;">Receive useful notifications through our customizable emailing feature. Customize the frequency of emails and which
       repos you want to notify for. </p>
     <hr class="my-4" style="color: black">
-    <p class="fs-2" style="color: white;">A Package Update Notifier that can make developer lives easier!</p>
+    <p class="fs-2" style="color: white;">A Package Update Notifier that can make developer lives easier!</p>  
     <p class="lead">
     </p>
 </div>
