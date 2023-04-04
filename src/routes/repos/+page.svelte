4ac43b4496a4027bc5d811df8ee1849b396ba252
Erik Smith <eriksmith98@gmail.com>
2023-04-04 00:26:08 -0400

    added opt-in/opt-out functionality to repos page

--- src/routes/repos/+page.svelte
+++ src/routes/repos/+page.svelte
@@ -11,13 +11,11 @@
 		} else {
 			return;
 		}
-		// console.log(userName);
-		// let { data } = await octokit.request('GET /users/{username}/repos', {
-		// 	username: userName,
-		// 	headers: {}
-		// });
-		// return data;
-		return userName;
+		let { data }: any = await octokit.request('GET /users/{username}/repos', {
+			username: userName,
+			headers: {}
+		});
+		return data;
 	}
 
 	// pull all rows from backend response
@@ -48,29 +46,24 @@
     }
 
     //pull all rows from repo settings
-    async function pullSettings() {
+    async function pullSettings(repo_id: string) {
         let { data, error } = await supabase
             .from('repo_settings')
             .select('*')
             //add this to only get settings for a specific repo
-            // .eq('repo_id', repoIdAsVariable)
+            .eq('repo_id', repo_id)
 
             var repo_settings = <repo_settings[]>data;
-
-            return repo_settings;
+			console.log(repo_settings[0]);
+            return repo_settings[0];
     }
 
 	//create a new repo_setting row
-    const theRepoId = "";
-    const email = "";
-    const theExcludedPackages = "";
-    const notifreq = "";
-    const notifday = "";
-    async function createNewSetting() {
+    async function createNewSetting(repo_id: string) {
         const { data, error } = await supabase
         .from('repo_settings')
         .insert([
-        { repo_id: theRepoId, email: email, excluded_packages: theExcludedPackages, notification_frequency: notifreq, notification_day : notifday  },
+        { repo_id: repo_id, email: user?.email, excluded_packages: null, notification_frequency: "Weekly", notification_day : "Monday"  },
         ])
     }
 
@@ -81,24 +74,61 @@
   			.update({ excluded_packages: 'otherValue' })
   			.eq('repo_id', 123456789)
 	}
-
-	function repoCheckboxHandler(event: any) {
-		if (event.target.checked) {
-			var repoId = event.target.parentElement.parentElement.id;
-
-			alert(repoId);
-		}
+	
+	async function deleteSetting(repo_id: string) {
+		const { error } = await supabase
+  			.from('repo_settings')
+  			.delete()
+  			.eq('repo_id', repo_id)
 	}
 	
-	function pkgCheckboxHandler(event: any) {
-		if (event.target.checked) {
-			var repoId = event.target.parentElement.parentElement.parentElement.id;
-			var pkgName = event.target.name;
+	async function saveChanges() {
+		var checkboxes = document.querySelectorAll('input[type=checkbox]')
 
-			alert(repoId + pkgName);
+		for (var i = 0; i < checkboxes.length; i++) {
+			var repo = checkboxes[i] as HTMLInputElement;
+			var settings = await pullSettings(repo.id);
+			
+			// when checkbox is checked
+			if (repo.checked) {
+				if (settings) {
+					console.log("Settings already exist!");
+				}
+				else {
+					createNewSetting(repo.id);
+					console.log("Opting in...");
+				}
+			}
+			else {
+				if (settings) {
+					deleteSetting(repo.id);
+					console.log("Opting out...");
+				}
+				else {
+					console.log("No settings to delete!");
+				}
+			}
 		}
 	}
+
+	// function repoCheckboxHandler(event: any) {
+	// 	if (event.target.checked) {
+	// 		var repoId = event.target.id;
+
+	// 		alert(repoId);
+	// 	}
+	// }
+	
+	// function pkgCheckboxHandler(event: any) {
+	// 	if (event.target.checked) {
+	// 		var repoId = event.target.parentElement.parentElement.parentElement.id;
+	// 		var pkgName = event.target.name;
+
+	// 		alert(repoId + pkgName);
+	// 	}
+	// }
 	// console.log(pullPackages(123456789));
+
 	// console.log(pullSettings());
 
 </script>
@@ -113,24 +143,29 @@
 </div>
 
 <main>
-	<div class="container text-center fs-3" style="color: white;"><b>{#await userTest() then userName}{userName}'s {/await}Repos</b>
-		{#await pullBackendResponse() then backend_reponse}
-			{#each backend_reponse as repo}
-			<div id={repo.repo_id}>
+	<div class="container text-center fs-3" style="color: white;">
+		{#await userTest() then repos}<b>{repos[0].owner.login}'s Repos</b>
+		<!-- {#await pullBackendResponse() then backend_reponse} -->
+		<div id="list">
+			{#each repos as repo}
 			<!-- default checkbox for each repo starts unchecked -->
 				<div class="form-check">
-					<input class="form-check-input" type="checkbox" value="" id={String(repo.id)} on:change={repoCheckboxHandler}>
-					<label class="form-check-label" for={String(repo.id)}>
-					Repo ID: {repo.repo_id}
+					{#await pullSettings(repo.id) then settings}
+						{#if settings}	
+							<input class="form-check-input" type="checkbox" value="" id={repo.id} checked>
+						{:else}
+							<input class="form-check-input" type="checkbox" value="" id={repo.id}>
+						{/if}
+					{/await}
+					<label class="form-check-label" for={repo.id}>
+					{repo.name}
 					</label>
 			  	</div>
-				{#await pullPackages(repo.id) then package_info}
+				<!-- {#await pullPackages(repo.id) then package_info}
 					{#each package_info as pkg}
-						<!-- each pkg unchecked -->
 						
 						<div class="row" style="">
 							<div class="form-check col" style="">
-								<!-- use string concatenation to give unique id for each package -->
 								<input class="form-check-input" type="checkbox" checked value="" name={pkg.package_name} id={String(pkg.response_id) + String(pkg.id)} on:change={pkgCheckboxHandler}>
 								<label class="form-check-label" for={String(pkg.response_id) + String(pkg.id)}>
 									<div class="">Package name: {pkg.package_name}</div>
@@ -140,10 +175,12 @@
 							<div class="col">Most recent version: {pkg.current_version}</div>
 						</div>
 						{/each}
-						{/await}
-			</div>
+						{/await} -->
 			{/each}
+		</div>
 		{/await}
+
+		<button class="btn btn-primary" type="submit" on:click={saveChanges}>Save changes</button>
 		<!-- {#await main() then data}
   			{#each data as repository}
     		<div class="row">
